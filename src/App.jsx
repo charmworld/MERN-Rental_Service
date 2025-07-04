9495ceed725e7eaf1659d32dcb85100460e76560
charmworld <charmi245129@gmail.com>
2025-07-04 12:29:20 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 
 
-import React from 'react'
-import Navbar from './components/Navbar'
+import React, { useState} from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom';
+
+import CarDetails from './pages/CarDetails';
+import Home from './pages/Home';
+import Cars from './pages/Cars';
+import Navbar from './components/Navbar';
+import Bookings from './pages/Bookings';
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+
   return (
-    <div> 
-      <Navbar/>
-    </div>
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/car-details/:id' element={<CarDetails/>}/>
+        <Route path='/cars' element={<Cars/>}/>
+        <Route path='/bookings' element={<Bookings/>}/>
+      </Routes>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import React, {useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import {assets, menuLinks} from '../assets/assets';
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const location = useLocation()
     const [open, setOpen] = useState(false)
-    
+    const navigate = useNavigate()
+
+
   return (
     <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24
     xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all
     ${location.pathname === "/" && "bg-light"}`}>
 
         <Link to='/'>
-       <img src={assets.logo}  alt="logo"  className="h-8"/>
+       <img src={assets.mylogoo}  alt="logo"  className="h-6"/>
         </Link>
 
         <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16
@@ -29,7 +31,25 @@ const Navbar = () => {
 
             ))
               }
+              <div className='hideen lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56'>
+                <input type="text" className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" placeholder="Search cars"/>
+                <img src={assets.search_icon} alt="search" />
+
+              </div>
+
+              <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
+                <button onClick={()=> navigate('/owner')} 
+                className="cursor-pointer"> Dashboard </button>
+                <button onClick={()=> setShowLogin(true)}className="cursor-pointer px-8 py-2 bg-primary
+                hover:bg-primary-dull transition-all text-white rounded-lg">Login</button>
+              </div>
+
+
         </div>
+        <button className='sm:hidden cursor-pointer' aria-label="Menu" 
+        onClick={() => setOpen(!open)}>
+            <img src={open ? assets.close_icon : assets.menu_icon} alt="menu" />
+        </button>
 
     </div>
   )
